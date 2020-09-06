294bc822b80d117070b2819bcddd6fb5657ee774
nguyenethan01 <nguyenethan01@gmail.com>
2020-09-06 14:25:42 -0700

    Implement Algorithm

---
+++ towers.py
@@ -0,0 +1,11 @@
+def towers_of_hanoi (n , source, destination, buffer):
+  #base case: move last piece to destiation
+  if n==1: 
+      print (f'Move disk 1 from source {source} to destination {destination} ')
+      return
+  #recursive case:
+  towers_of_hanoi(n-1, source, buffer, destination) 
+  print (f'Move disk {n} from source {source} to destination {destination} ')
+  towers_of_hanoi(n-1, buffer, destination, source) 
+
+towers_of_hanoi(8,'A','B','C')
